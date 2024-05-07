b8e97363e84fd4523204a46a2743b2ac8de153f3
Usman Akinyemi <usmanakinyemi202@gmail.com>
2024-05-07 10:12:46 -0400

    Used eslint to fix some linting issues

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,5 +1,5 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let [idx, _] of array.entries()) {
+  for (const [idx, _] of array.entries()) {
     let value = array[idx];
     array[idx] = appendString + value;
   }
